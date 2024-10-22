08e952e86aedb25361687980ddeec0e826c55bf1
TIMOTHE ESPOSITO <timotheesposito@gmail.com>
2024-10-22 17:13:34 +0200

    React-basics-3

---
+++ src/components.1/PokemonCard.tsx
@@ -0,0 +1,24 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+function PokemonCard() {
+    const pokemon = pokemonList[1]
+    return (
+    <figure>
+        {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>) }
+        <figcaption>
+            <p>{pokemon.name}</p>
+        </figcaption>
+
+    </figure>);
+  }
+
+  
+  export default PokemonCard;
